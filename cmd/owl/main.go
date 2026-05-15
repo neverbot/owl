0674e8d824cc0f71dc6dae862632c92542367a05
neverbot <ivan@neverbot.com>
2026-05-15 05:33:26 +0200

    feat(cmd): wire config loader with --config and --check-config

--- cmd/owl/main.go
+++ cmd/owl/main.go
@@ -5,18 +5,45 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/neverbot/owl/internal/config"
 	"github.com/neverbot/owl/internal/version"
 )
 
 func main() {
-	showVersion := flag.Bool("version", false, "print version and exit")
+	var (
+		configPath  string
+		showVersion bool
+		checkOnly   bool
+	)
+	flag.StringVar(&configPath, "config", "/etc/owl/config.yml", "path to the config file")
+	flag.BoolVar(&showVersion, "version", false, "print version and exit")
+	flag.BoolVar(&checkOnly, "check-config", false, "validate the config and exit")
 	flag.Parse()
 
-	if *showVersion {
+	if showVersion {
 		fmt.Println(version.String())
-		os.Exit(0)
+		return
 	}
 
-	fmt.Fprintln(os.Stderr, "owl: no subcommand selected; run with --version")
-	os.Exit(2)
+	cfg, err := config.Load(configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "owl: %v\n", err)
+		os.Exit(2)
+	}
+	config.ApplyEnv(&cfg)
+	if err := config.Validate(&cfg); err != nil {
+		fmt.Fprintf(os.Stderr, "owl: %v\n", err)
+		os.Exit(2)
+	}
+
+	if checkOnly {
+		fmt.Fprintf(os.Stderr, "owl: config %s OK\n", configPath)
+		return
+	}
+
+	mgr := config.NewManager(cfg)
+	_ = mgr // wired into subsequent subsystems in later plans
+
+	fmt.Fprintln(os.Stderr, "owl: foundation only; no runtime subsystems wired yet")
+	os.Exit(0)
 }
